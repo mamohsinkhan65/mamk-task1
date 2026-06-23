8224aeb0a09e0d04893dfb7b7901b270f8ff85bb
mamohsinkhan65 <mamohsinkhan65@gmail.com>
2026-06-23 13:27:39 +0100

    Create jenkins-install.sh
    mohsin set of command to install Jenkins and view password to access it at the end

---
+++ jenkins-install.sh
@@ -0,0 +1,11 @@
+#Installing Java 21
+sudo apt install -y openjdk-21-jre openjdk-21-jdk
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install -y jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+#jenkins password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
