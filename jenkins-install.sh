59008833374f028f2d491c933350bc749d0df649
mamohsinkhan65 <mamohsinkhan65@gmail.com>
2026-06-23 13:46:27 +0100

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,4 +1,4 @@
-#Installing Java 21
+#Short script to install Java and jenkins in one script Installing Java 21 personal user 
 sudo apt install -y openjdk-21-jre openjdk-21-jdk
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
